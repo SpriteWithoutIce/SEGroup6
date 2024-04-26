b602df178ec371efa00739ef8712ad970a7867c4
Lyx-Roisy <2059218150@qq.com>
2024-04-27 02:59:53 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <BillDetails ref="BillDetails"> </BillDetails>
+  <HeaderNavigation />
+  <BillList />
+  <NavBar />
+</template>
+
+<script>
+import BillDetails from './components/Bills/BillDetails.vue'
+import BillList from './components/Bills/BillList.vue'
+import HeaderNavigation from './components/HeaderNavigation.vue';
+import NavBar from './components/Navigation/Navbar.vue'
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    BillDetails,
+    BillList,
+    HeaderNavigation,
+  },
+  methods: {
+    showBillDetails () {
+      this.$refs.billDetails.openModal();
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+// import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(ElementPlus) // 全局注册 Element Plus 组件
+app.mount('#app')
