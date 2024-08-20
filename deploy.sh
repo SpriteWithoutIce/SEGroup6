e66822cc3860b9b084da3394c21b0c998b744751
Keith1276 <141840126+Keith1276@users.noreply.github.com>
2024-08-20 16:06:25 +0800

    部署2

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ FRONTEND_PATH="猫猫就诊/Project/frontend/dist"  # 前端静态文件的部�
 BACKEND_PATH="猫猫就诊/Project"    # 后端代码的部署路径
 UWSGI_SERVICE_NAME="uwsgi"  # uWSGI 服务名
 NGINX_SERVICE_NAME="nginx"  # Nginx 服务名
-SSH_PRIVATE_KEY="${{ secrets.SSH_PRIVATE_KEY }}"  # 这里替换成SSH Key
+SSH_PRIVATE_KEY="22371468Se"  # 这里替换成SSH Key
 
 # 设置 SSH 配置
 mkdir -p ~/.ssh
