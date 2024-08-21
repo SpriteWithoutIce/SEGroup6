75b76de09c270fc9b5e86f47816f3c4774452e05
可乐要加冰 <lyh1851855@163.com>
2024-08-21 09:07:16 +0800

    complete setting

--- 猫猫就诊/Project/app01/delete.py
+++ 猫猫就诊/Project/app01/delete.py
@@ -1,3 +1,7 @@
 from django.db import connection
+from ..backend.settings import DATABASES
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 with connection.cursor() as cursor:
     cursor.execute("DROP DATABASE IF EXISTS test_seproject;")
