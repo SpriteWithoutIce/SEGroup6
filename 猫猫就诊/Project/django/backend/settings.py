7e75bb4c725dc84e27e78dae15f09f55dd99605a
berry0303 <142763755+berry0303@users.noreply.github.com>
2024-08-21 19:36:58 +0800

    Docker-new

--- 猫猫就诊/Project/django/backend/settings.py
+++ 猫猫就诊/Project/django/backend/settings.py
@@ -125,12 +125,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "../frontend/dist/static"),
-    os.path.join(BASE_DIR, 'static/images/medicine'),
-    os.path.join(BASE_DIR, 'static/images/doctors')
-]
+# STATIC_URL = "static/"
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "../frontend/dist/static"),
+#     os.path.join(BASE_DIR, 'static/images/medicine'),
+#     os.path.join(BASE_DIR, 'static/images/doctors')
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
