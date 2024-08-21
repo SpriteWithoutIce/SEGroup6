1517bfd352f2a99fe892738eec8e00ccc78fac88
Keith1276 <141840126+Keith1276@users.noreply.github.com>
2024-08-21 13:56:36 +0800

    上班

--- deploy.sh
+++ deploy.sh
@@ -39,6 +39,7 @@ sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_
     # 重启 uWSGI 和 Nginx 服务
     echo "启动 uWSGI 和nginx服务..."
     sudo killall -9 uwsgi
+    pwd
     sudo uwsgi --ini uwsgi.ini
     sudo nginx -s reload
     #sudo /etc/init.d/uwsgi restart
