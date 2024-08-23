e43da07219350d0e5658f435244054772d120421
smellycat111 <2740723076@qq.com>
2024-08-23 16:25:30 +0800

    微服务未老实

--- 猫猫就诊/Project/deploy.sh
+++ 猫猫就诊/Project/deploy.sh
@@ -51,6 +51,7 @@ sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_
 
     cd SEGroup6/猫猫就诊/Project
     ls
+    cd ..
     echo "$SSH_PASSWORD" | sudo killall -9 nginx
     echo "$SSH_PASSWORD" | sudo docker-compose up -d --remove-orphans
 
