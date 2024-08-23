e7fea2fa761cc63cf6fea0d1fb8c14cd3d24dc54
berry0303 <142763755+berry0303@users.noreply.github.com>
2024-08-23 11:42:25 +0800

    Docker

--- 猫猫就诊/Project/frontend/vue.config.js
+++ 猫猫就诊/Project/frontend/vue.config.js
@@ -2,12 +2,12 @@ module.exports = {
     // 配置跨域请求
     devServer: {
         port: 80,
-        host: 'localhost',
+        host: '127.0.0.1',
         open: true,
         https: false,
         proxy: {
             '/api': {
-                target: 'http://localhost:5000/',
+                target: 'http://127.0.0.1:5000/',
                 ws: true,
                 changeOrigin: true,
                 pathRewrite: {
