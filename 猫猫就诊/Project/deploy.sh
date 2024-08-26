1b7aefb8c0e55e2c3fa37e5449e128658563d227
RoisyL <2059218150@qq.com>
2024-08-26 10:03:59 +0800

    Update deploy.sh

--- 猫猫就诊/Project/deploy.sh
+++ 猫猫就诊/Project/deploy.sh
@@ -55,8 +55,8 @@ sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_
     # 进入后端项目目录
     cd ./SEGroup6/猫猫就诊/Project
     ls
-    # echo "$SSH_PASSWORD" | sudo killall -9 uwsgi
-    # echo "$SSH_PASSWORD" | sudo killall -9 nginx
+    echo "$SSH_PASSWORD" | sudo killall -9 uwsgi
+    echo "$SSH_PASSWORD" | sudo killall -9 nginx
     echo "$SSH_PASSWORD" | docker-compose down
     echo "$SSH_PASSWORD" | docker build -t my-django-app .
     echo "$SSH_PASSWORD" | docker-compose build --no-cache
