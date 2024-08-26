cf9bcc650ca7f1ecd421faa2aa4a72fa677b1eb7
可乐要加冰 <lyh1851855@163.com>
2024-08-26 16:10:46 +0800

    Update deploy.sh

--- 猫猫就诊/Project/deploy.sh
+++ 猫猫就诊/Project/deploy.sh
@@ -3,9 +3,9 @@
 # 配置项
 SERVER_USER="ubuntu"
 SERVER_IP="81.70.182.21"
-REQUIREMENTS_PATH="SEGroup6/requirements.txt"
-FRONTEND_PATH="SEGroup6/猫猫就诊/Project/frontend/dist"  # 前端静态文件的部署路径
-BACKEND_PATH="SEGroup6/猫猫就诊/Project/django"    # 后端代码的部署路径
+REQUIREMENTS_PATH="SEGroup6_lyh/requirements.txt"
+FRONTEND_PATH="SEGroup6_lyh/猫猫就诊/Project/frontend/dist"  # 前端静态文件的部署路径
+BACKEND_PATH="SEGroup6_lyh/猫猫就诊/Project/django"    # 后端代码的部署路径
 UWSGI_SERVICE_NAME="uwsgi"  # uWSGI 服务名
 NGINX_SERVICE_NAME="nginx"  # Nginx 服务名
 UWSGI_INI_PATH="uwsgi.ini"  # uWSGI 的 ini 文件路径
@@ -15,7 +15,7 @@ SSH_PASSWORD="Lyh20050316"  #
 
 echo "checkout branch"
 sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP << EOF
-    cd SEGroup6
+    cd SEGroup6_lyh
     git reset --hard
     git clean -fd
     git checkout microservice
@@ -30,30 +30,30 @@ sudo apt-get update && sudo apt-get install -y sshpass
 sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/frontend/dist/ $SERVER_USER@$SERVER_IP:$FRONTEND_PATH
 # 后端部署
 echo "Starting back-end deployment..."
-sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/administrator_service/ $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/administrator_service
-sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/doctor_service/ $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/doctor_service
-sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/user_service/ $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/user_service
-sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/patient_service/ $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/patient_service
+sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/administrator_service/ $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/administrator_service
+sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/doctor_service/ $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/doctor_service
+sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/user_service/ $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/user_service
+sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/patient_service/ $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/patient_service
 echo "传dockerfile和配置文件"
-# sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/doctor_service/Dockerfile $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/doctor_service/Dockerfile
-# sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/user_service/Dockerfile $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/user_service/Dockerfile
-# sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/patient_service/Dockerfile $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/patient_service/Dockerfile
-# sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/administrator_service/Dockerfile $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/administrator_service/Dockerfile
-sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/Dockerfile $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/Dockerfile
-sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/requirements.txt $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/requirements.txt
-sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/frontend/Dockerfile $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/frontend/Dockerfile
-sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/frontend/nginx.conf $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/frontend/nginx.conf
+# sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/doctor_service/Dockerfile $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/doctor_service/Dockerfile
+# sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/user_service/Dockerfile $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/user_service/Dockerfile
+# sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/patient_service/Dockerfile $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/patient_service/Dockerfile
+# sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/administrator_service/Dockerfile $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/administrator_service/Dockerfile
+sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/Dockerfile $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/Dockerfile
+sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/requirements.txt $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/requirements.txt
+sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/frontend/Dockerfile $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/frontend/Dockerfile
+sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/frontend/nginx.conf $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/frontend/nginx.conf
 sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/frontend/nginx.conf $SERVER_USER@$SERVER_IP:/etc/nginx/nginx.conf
-sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/frontend/default.conf $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/frontend/default.conf
+sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/frontend/default.conf $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/frontend/default.conf
 sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/frontend/default.conf $SERVER_USER@$SERVER_IP:/etc/nginx/conf.d/default.conf
-sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/frontend/uwsgi_params $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/frontend/uwsgi_params
-sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/docker-compose.yml $SERVER_USER@$SERVER_IP:SEGroup6/猫猫就诊/Project/docker-compose.yml
+sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/frontend/uwsgi_params $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/frontend/uwsgi_params
+sshpass -p "$SSH_PASSWORD" rsync -avz -e "ssh -o StrictHostKeyChecking=no" --progress ./猫猫就诊/Project/docker-compose.yml $SERVER_USER@$SERVER_IP:SEGroup6_lyh/猫猫就诊/Project/docker-compose.yml
 # SSH 到服务器上，执行后续命令
 echo "Connecting to server to finalize deployment..."
 sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP << EOF
     # 进入后端项目目录
 
-    cd SEGroup6/猫猫就诊/Project
+    cd SEGroup6_lyh/猫猫就诊/Project
     ls
     echo "$SSH_PASSWORD" | sudo killall -9 uwsgi
     echo "$SSH_PASSWORD" | sudo killall -9 nginx
